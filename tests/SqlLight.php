7d5fff4558809f240e18b3bbda446eb2c4ace39c
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-04-29 21:12:42 +0300

    Update SqlLight.php

--- tests/SqlLight.php
+++ tests/SqlLight.php
@@ -4,21 +4,24 @@
 	use PHPUnit\Framework\TestCase;
 	use Traineratwot\PDOExtended\Dsn;
 	use Traineratwot\PDOExtended\exceptions\DsnException;
+	use Traineratwot\PDOExtended\exceptions\PDOEException;
 	use Traineratwot\PDOExtended\PDOE;
 
 	class SqlLight extends TestCase
 	{
 		/**
-		 * @throws DsnException
+		 * @throws DsnException|PDOEException
 		 */
 		public function setUp()
 		: void
 		{
 			parent::setUp();
 			$sqLight = __DIR__ . '/test.db';
+			unset($this->db);
 			if (file_exists($sqLight)) {
 				unlink($sqLight);
 			}
+
 			$f = fopen($sqLight, 'wb');
 			fclose($f);
 
@@ -41,5 +44,11 @@
 		{
 			$tables = $this->db->getTablesList();
 			$this->assertEquals('test', $tables[0], 'getAllTables');
+		}
+
+		public function testTableExists()
+		{
+			$table = $this->db->tableExists('TeSt');
+			$this->assertEquals('test', $table, 'tableExists');
 		}
 	}
