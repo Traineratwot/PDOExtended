cba4faa84db5c9eeb2027070edd69a3e3d742fe3
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-05-16 12:22:27 +0300

    1

--- src/tableInfo/dataType/TFloat.php
+++ src/tableInfo/dataType/TFloat.php
@@ -3,6 +3,7 @@
 	namespace Traineratwot\PDOExtended\tableInfo\dataType;
 
 	use Traineratwot\PDOExtended\abstracts\DataType;
+	use Traineratwot\PDOExtended\exceptions\DataTypeException;
 
 	class TFloat extends DataType
 	{
@@ -11,10 +12,21 @@
 		/**
 		 * @inheritDoc
 		 */
-		public function validate()
+		public function validate($value)
 		: void
 		{
-			// TODO: Implement validate() method.
+			if ($this->canBeNull && empty($value)) {
+				return;
+			}
+			if ($this->canBeNull && strtolower($value) === 'null') {
+				return;
+			}
+			if (!$this->canBeNull && is_null($value)) {
+				throw new DataTypeException("can`t be null");
+			}
+			if (!is_numeric($value) || !is_float($value + 0) || !is_int($value + 0)) {
+				throw new DataTypeException("invalid float");
+			}
 		}
 
 		/**
@@ -22,6 +34,6 @@
 		 */
 		public function convert($value)
 		{
-			return is_null($value) ? NULL : (float)$value;
+			return (is_null($value) || $value === 'null') ? NULL : (float)$value;
 		}
 	}
--- src/tableInfo/dataType/TInt.php
+++ src/tableInfo/dataType/TInt.php
@@ -8,14 +8,24 @@
 	class TInt extends DataType
 	{
 		public string $phpName = 'int';
+
 		/**
 		 * @inheritDoc
 		 */
 		public function validate($value)
 		: void
 		{
-			if (($value && $this->canBeNull) && (!is_numeric($value) || !is_int($value + 0))) {
-				throw new DataTypeException("invalid string");
+			if ($this->canBeNull && empty($value)) {
+				return;
+			}
+			if ($this->canBeNull && strtolower($value) === 'null') {
+				return;
+			}
+			if (!$this->canBeNull && is_null($value)) {
+				throw new DataTypeException("can`t be null");
+			}
+			if (!is_numeric($value) || !is_int($value + 0)) {
+				throw new DataTypeException("invalid int");
 			}
 		}
 
@@ -24,6 +34,6 @@
 		 */
 		public function convert($value)
 		{
-			return is_null($value) ? NULL : (int)$value;
+			return (is_null($value) || $value === 'null') ? NULL : (int)$value;
 		}
 	}
