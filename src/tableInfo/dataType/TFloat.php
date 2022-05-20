a194a637add9c7c263dd0b0ba2a15b3aac0266d3
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-05-21 00:48:59 +0300

    1

--- src/tableInfo/dataType/TFloat.php
+++ src/tableInfo/dataType/TFloat.php
@@ -24,7 +24,7 @@
 			if (!$this->canBeNull && is_null($value)) {
 				throw new DataTypeException("can`t be null");
 			}
-			if (!is_numeric($value) || !is_float($value + 0) || !is_int($value + 0)) {
+			if (!is_numeric($value) && !is_float($value + 0) && !is_int($value + 0)) {
 				throw new DataTypeException("invalid float");
 			}
 		}
